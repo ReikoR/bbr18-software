9c9ee6c817942253156eccf9604ebb21a648616e
Reiko Randoja <reiko.randoja@gmail.com>
2018-11-08 23:42:37 +0200

    Change mainboard component to work with referee command data

--- mainboard/mainboard.js
+++ mainboard/mainboard.js
@@ -6,6 +6,22 @@ const publicConf = require('./public-conf');
 const mbedPort = publicConf.mbedPort;
 const mbedAddress = publicConf.mbedIpAddress;
 
+/**
+ * @typedef {Object} CommandObject
+ * @property {number[]} speeds
+ * @property {string} fieldID
+ * @property {string} robotID
+ * @property {boolean} shouldSendAck
+ */
+
+const commandBuffer = Buffer.alloc(13);
+const defaultCommandObject =  {
+    speeds: [0, 0, 0, 0, 0],
+    fieldID: 'Z',
+    robotID: 'Z',
+    shouldSendAck: false,
+};
+
 socketMainboard.on('error', (err) => {
     console.log(`socketMainboard error:\n${err.stack}`);
     socketMainboard.close();
@@ -19,7 +35,7 @@ socketMainboard.on('listening', () => {
     const address = socketMainboard.address();
     console.log(`socketMainboard listening ${address.address}:${address.port}`);
 
-    sendCommandToMainboard([0, 0, 0, 0, 0]);
+    sendCommandToMainboard(defaultCommandObject);
 });
 
 socketMainboard.bind(publicConf.mbedPort, () => {
@@ -68,6 +84,21 @@ function close() {
     });
 }
 
+/**
+ * @param {CommandObject} commandObject
+ */
+function updateCommandBuffer(commandObject) {
+    const speeds = commandObject.speeds;
+
+    for (let i = 0; i < speeds.length; i++) {
+        commandBuffer.writeInt16LE(speeds[i], 2 * i);
+    }
+
+    commandBuffer.writeUInt8(commandObject.fieldID.charCodeAt(0), 10);
+    commandBuffer.writeUInt8(commandObject.robotID.charCodeAt(0), 11);
+    commandBuffer.writeUInt8(commandObject.shouldSendAck ? 1 : 0, 12);
+}
+
 function handleInfo(info, address, port) {
     console.log('handleInfo', info);
     if (info.topic === 'mainboard_command') {
@@ -75,30 +106,13 @@ function handleInfo(info, address, port) {
     }
 }
 
-function sendToMainboard(command) {
-    const message = Buffer.from(command);
-    //console.log('send:', command, 'to', '192.168.4.1', 8042);
-
-    socketMainboard.send(message, mbedPort, mbedAddress, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-}
-
 /**
- *
- * @param {Array.<number>} speeds
+ * @param {CommandObject} command
  */
-function sendCommandToMainboard(speeds) {
-    const command = new Int16Array(5);
+function sendCommandToMainboard(command) {
+    updateCommandBuffer(command);
 
-    for (let i = 0; i < speeds.length && i < command.length; i++) {
-        command[i] = speeds[i];
-    }
-
-    let message = new Buffer.from(command.buffer);
-    socketMainboard.send(message, 0, message.length, mbedPort, mbedAddress);
+    socketMainboard.send(commandBuffer, 0, commandBuffer.length, mbedPort, mbedAddress);
 }
 
 function handleMainboardMessage(message) {
@@ -112,7 +126,8 @@ function handleMainboardMessage(message) {
         ball2: message.readUInt8(11) === 1,
         distance: message.readUInt16LE(12),
         isSpeedChanged: message.readUInt8(14) === 1,
-        time: message.readInt32LE(15)
+        refereeCommand: String.fromCharCode(message.readUInt8(15)),
+        time: message.readInt32LE(16)
     };
 
     console.log(data);
